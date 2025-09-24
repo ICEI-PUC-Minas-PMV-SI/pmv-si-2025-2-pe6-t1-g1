ed454ecc7c863dd276fd6920f3cfb8c5721d12a5
Bruno Tetzner <brunoltetzner@gmail.com>
2025-09-23 21:51:16 -0300

    feat: add sql script

--- web_api/Migrations/20250924003322_RenameUserCarrinhoToUserCart.cs
+++ web_api/Migrations/20250924003322_RenameUserCarrinhoToUserCart.cs
@@ -8,24 +8,19 @@ namespace web_api.Migrations
     {
         protected override void Up(MigrationBuilder migrationBuilder)
         {
-            // Rename table without losing data
             migrationBuilder.Sql("EXEC sp_rename 'UserCarrinhos', 'UserCarts'");
             
-            // Rename primary key constraint
             migrationBuilder.Sql("EXEC sp_rename 'PK_UserCarrinhos', 'PK_UserCarts'");
             
-            // Rename foreign key constraints
             migrationBuilder.Sql("EXEC sp_rename 'FK_UserCarrinhos_Items_ItemId', 'FK_UserCarts_Items_ItemId'");
             migrationBuilder.Sql("EXEC sp_rename 'FK_UserCarrinhos_Users_UserId', 'FK_UserCarts_Users_UserId'");
             
-            // Rename indexes with correct syntax
             migrationBuilder.Sql("EXEC sp_rename 'UserCarts.IX_UserCarrinhos_ItemId', 'IX_UserCarts_ItemId', 'INDEX'");
             migrationBuilder.Sql("EXEC sp_rename 'UserCarts.IX_UserCarrinhos_UserId_ItemId', 'IX_UserCarts_UserId_ItemId', 'INDEX'");
         }
 
         protected override void Down(MigrationBuilder migrationBuilder)
         {
-            // Reverse rename operations
             migrationBuilder.Sql("EXEC sp_rename 'UserCarts.IX_UserCarts_ItemId', 'IX_UserCarrinhos_ItemId', 'INDEX'");
             migrationBuilder.Sql("EXEC sp_rename 'UserCarts.IX_UserCarts_UserId_ItemId', 'IX_UserCarrinhos_UserId_ItemId', 'INDEX'");
             migrationBuilder.Sql("EXEC sp_rename 'FK_UserCarts_Items_ItemId', 'FK_UserCarrinhos_Items_ItemId'");
